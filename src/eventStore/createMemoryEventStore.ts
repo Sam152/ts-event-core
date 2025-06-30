39d92673849e6e58f90f6f78e83ae8b3f2e4317a
Sam <54m152@gmail.com>
2025-06-30 21:44:54 +0800

    Progress

--- src/eventStore/createMemoryEventStore.ts
+++ src/eventStore/createMemoryEventStore.ts
@@ -1,20 +1,34 @@
 import { EventEnvelope, EventStore } from "./EventStore.ts";
+import { UniqueConstraintViolationError } from "./UniqueConstraintViolationError.ts";
 
 export function createMemoryEventStore(): EventStore<unknown> {
-  /**
-   * A map of aggregate IDs to an array of events.
-   */
   const storage: Record<string, EventEnvelope<unknown>[]> = {};
+  const createKey = (aggregateType: string, aggregateId: string) => `${aggregateType}:${aggregateId}`;
 
   return {
-    persist: async (events) => {
+    persist: (events) => {
+      events.forEach((event) => {
+        const key = createKey(event.aggregateType, event.aggregateId);
+        const existingEvents = storage[key] || [];
+
+        if (existingEvents.some((existing) => existing.aggregateVersion === event.aggregateVersion)) {
+          throw new UniqueConstraintViolationError();
+        }
+
+        storage[key].push(event);
+      });
+      return Promise.resolve();
     },
-    retrieve: async ({
+    retrieve: ({
       aggregateType,
       aggregateId,
       fromVersion,
     }) => {
-      return [];
+      const key = createKey(aggregateType, aggregateId);
+      const events = storage[key] || [];
+      return Promise.resolve(
+        fromVersion !== undefined ? events.filter((event) => event.aggregateVersion >= fromVersion) : events,
+      );
     },
   };
 }
