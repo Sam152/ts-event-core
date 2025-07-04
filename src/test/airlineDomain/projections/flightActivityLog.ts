c3f0419059a3965999d431e49292617cc8fa69ea
Sam <54m152@gmail.com>
2025-07-04 17:54:56 +0800

    Progress

--- src/test/airlineDomain/projections/flightActivityLog.ts
+++ src/test/airlineDomain/projections/flightActivityLog.ts
@@ -14,5 +14,31 @@ type FlightActivityLog = {
 };
 
 export function flightActivityLog(state: FlightActivityLog, event: AirlineEvent): FlightActivityLog {
-  
+  switch (event.payload.type) {
+    case "PASSENGER_BOARDED": {
+      const passengerKey = `${event.payload.passengerName}:${event.payload.passportNumber}`;
+      return {
+        ...state,
+        passengers: {
+          ...state.passengers,
+          [passengerKey]: {
+            flightsTaken: (state.passengers[passengerKey]?.flightsTaken || 0) + 1,
+          },
+        },
+      };
+    }
+    case "FLIGHT_DEPARTED": {
+      return {
+        ...state,
+        planes: {
+          ...state.planes,
+          [event.aggregateId]: {
+            tripsFlown: (state.planes[event.aggregateId]?.tripsFlown || 0) + 1,
+          },
+        },
+      };
+    }
+    default:
+      return state;
+  }
 }
