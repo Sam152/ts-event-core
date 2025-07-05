bd73379d9d2c53c5851477acca85af4565a3c776
Sam <54m152@gmail.com>
2025-07-05 12:59:37 +0800

    Progress

--- src/projection/Projector.ts
+++ src/projection/Projector.ts
@@ -1,1 +1,23 @@
-export type Projector = {};
+/**
+ * Projectors take a stream of events from an event store and transform them into
+ * useful data structures. These are often called read models.
+ *
+ * Read models are typically eventually consistent and thus are not required to
+ * adhere to any of the boundaries defined by the aggregate roots.
+ *
+ * New read models can be added at any point in time and then can be deleted after
+ * they are no longer useful.
+ *
+ * Projectors can be structured as reducers, or they can mutate data structures as they
+ * go. They may deal with data structures that describe all the events in the system as
+ * a whole or selectively choose to build smaller structures out of individual aggregates
+ * or other relations found within the event payload.
+ *
+ * They can build data structures that are in-memory, relational databases, speciality
+ * databases or any other system that provides utility over your data.
+ *
+ * For these reasons, the signature of a projector is extremely simple, the only contract
+ * an event sourced system needs to fulfil is providing a stream of events. How data can be
+ * retrieved or accessed beyond that, is entirely dependent on the use case.
+ */
+export type Projector<TEvent extends Event> = (event: TEvent) => void | Promise<void>;
