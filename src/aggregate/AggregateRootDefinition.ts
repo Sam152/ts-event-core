76982d7f8d1a98b6cc9989ff9e86e96102f4a415
Sam <54m152@gmail.com>
2025-07-06 20:04:44 +0800

    Progress

--- src/aggregate/AggregateRootDefinition.ts
+++ src/aggregate/AggregateRootDefinition.ts
@@ -1,12 +1,7 @@
-export type AggregateReducer<TState, TEvent> = (state: TState, event: TEvent) => TState;
-
-type CommandMap<TAggregateRootState, TEvent> = {
-  [key: string]: <TCommandData extends never>(
-    aggregate: TAggregateRootState,
-    commandData: TCommandData,
-  ) => TEvent | TEvent[];
-};
-
+/**
+ * An aggregate root definition, the state and commands used to power
+ * writes in an event sourced system.
+ */
 export type AggregateRootDefinition<TAggregateRootState, TEvent> = {
   commands: CommandMap<TAggregateRootState, TEvent>;
   state: {
@@ -15,4 +10,20 @@ export type AggregateRootDefinition<TAggregateRootState, TEvent> = {
   };
 };
 
-export type AggregateRootDefinitionMap = Record<string, AggregateRootDefinition<any, any>>;
+export type AggregateReducer<TState, TEvent> = (state: TState, event: TEvent) => TState;
+
+type CommandMap<TAggregateRootState, TEvent> = {
+  [key: string]: <TCommandData extends never>(
+      aggregate: TAggregateRootState,
+      commandData: TCommandData,
+  ) => TEvent | TEvent[];
+};
+
+
+export type AggregateRootDefinitionMap = Record<
+  string,
+  AggregateRootDefinition<
+    any,
+    any
+  >
+>;
