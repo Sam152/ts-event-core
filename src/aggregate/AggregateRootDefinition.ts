b9a8cdcef148490185e2e5ce37f7d2b254c226a5
Sam <54m152@gmail.com>
2025-07-06 20:13:56 +0800

    Progress

--- src/aggregate/AggregateRootDefinition.ts
+++ src/aggregate/AggregateRootDefinition.ts
@@ -14,14 +14,16 @@ export type AggregateReducer<TState, TEvent> = (state: TState, event: TEvent) =>
 
 type CommandMap<TAggregateRootState, TEvent> = {
   [key: string]: <TCommandData extends never>(
-      aggregate: TAggregateRootState,
-      commandData: TCommandData,
+    aggregate: TAggregateRootState,
+    commandData: TCommandData,
   ) => TEvent | TEvent[];
 };
 
-
 export type AggregateRootDefinitionMap = Record<
   string,
+  // How can this be typed correctly, given TAggregateRootState is required to be both
+  // a function argument and return type, so covariant/contravariant issues apply with
+  // using either unknown or never for this use case.
   AggregateRootDefinition<
     any,
     any
