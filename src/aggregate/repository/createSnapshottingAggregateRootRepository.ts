56dcbe1572b860eea9df2c8c9b8b82971fa2bd92
Sam <54m152@gmail.com>
2025-07-11 16:41:06 +0800

    Progress

---
+++ src/aggregate/repository/createSnapshottingAggregateRootRepository.ts
@@ -0,0 +1,56 @@
+import { Event, EventsRaisedByAggregateRoots, EventStore } from "../../eventStore/EventStore.ts";
+import { AggregateRootRepository } from "../AggregateRootRepository.ts";
+import { AggregateRootDefinitionMap, AggregateRootDefinitionMapTypes } from "../AggregateRootDefinition.ts";
+import { SnapshotStorage } from "../snapshot/SnapshotStorage.ts";
+
+/**
+ * ...
+ */
+export function createSnapshottingAggregateRootRepository<
+  TAggregateDefinitionMap extends AggregateRootDefinitionMap<TAggregateMapTypes>,
+  TAggregateMapTypes extends AggregateRootDefinitionMapTypes = AggregateRootDefinitionMapTypes,
+>(
+  { eventStore, aggregateRoots, snapshotStorage }: {
+    eventStore: EventStore<EventsRaisedByAggregateRoots<TAggregateDefinitionMap, TAggregateMapTypes>>;
+    aggregateRoots: TAggregateDefinitionMap;
+    snapshotStorage: SnapshotStorage<TAggregateDefinitionMap, TAggregateMapTypes>;
+  },
+): AggregateRootRepository<TAggregateDefinitionMap, TAggregateMapTypes> {
+  return {
+    retrieve: async (
+      { aggregateRootId, aggregateRootType },
+    ) => {
+      const definition = aggregateRoots[aggregateRootType];
+      const events = eventStore.retrieve({
+        aggregateRootId,
+        aggregateRootType: aggregateRootType as string,
+      });
+
+      let aggregateVersion: number | undefined = undefined;
+      let state = definition.state.initialState;
+      for await (const event of events) {
+        state = definition.state.reducer(state, event.payload);
+        aggregateVersion = event.aggregateVersion;
+      }
+
+      return {
+        aggregateRootId,
+        aggregateRootType,
+        aggregateVersion,
+        state,
+      };
+    },
+    persist: async ({ aggregate, pendingPayloads }) => {
+      const pendingEvents: Event[] = pendingPayloads.map(
+        (payload, i) => ({
+          aggregateRootType: aggregate.aggregateRootType as string,
+          aggregateRootId: aggregate.aggregateRootId,
+          recordedAt: new Date(),
+          aggregateVersion: (aggregate.aggregateVersion ?? 0) + (i + 1),
+          payload,
+        }),
+      );
+      await eventStore.persist(pendingEvents);
+    },
+  };
+}
