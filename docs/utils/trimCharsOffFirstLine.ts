3f0b0fb019c105e370a40c2408bbaa2c8dc5f502
Sam <54m152@gmail.com>
2025-07-26 18:31:45 +0800

    Progress

--- docs/utils/trimCharsOffFirstLine.ts
+++ docs/utils/trimCharsOffFirstLine.ts
@@ -1,8 +1,8 @@
 export function trimCharsOffFirstLine(numberOfChars: number) {
   return (str: string): string => {
     return str
-      .split('\n')
-      .map(line => line.slice(numberOfChars))
-      .join('\n');
+      .split("\n")
+      .map((line) => line.slice(numberOfChars))
+      .join("\n");
   };
 }
