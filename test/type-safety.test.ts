9f8c17b6d8a6552e046b040d928b3deb0a6f41a3
Sam <54m152@gmail.com>
2025-07-26 18:42:36 +0800

    Progress

--- test/type-safety.test.ts
+++ test/type-safety.test.ts
@@ -2,6 +2,7 @@ import { createImmediateCommandIssuer } from "../src/command/immediate/createImm
 import { airlineAggregateRoots, AirlineEvent } from "./airlineDomain/aggregateRoot/airlineAggregateRoots.ts";
 import { createMemoryEventStore } from "../src/eventStore/memory/createMemoryEventStore.ts";
 import { createBasicAggregateRootRepository } from "../src/aggregate/repository/createBasicAggregateRootRepository.ts";
+import { describe, it } from "jsr:@std/testing/bdd";
 
 const issueCommand = createImmediateCommandIssuer({
   aggregateRoots: airlineAggregateRoots,
@@ -11,42 +12,44 @@ const issueCommand = createImmediateCommandIssuer({
   }),
 });
 
-Deno.test("produces type errors given an invalid aggregate type", async () => {
-  const _params: Parameters<typeof issueCommand>[0] = {
-    // @ts-expect-error - this should produce a type error.
-    aggregateRootType: "NOT_AN_AGGREGATE",
-    // @ts-expect-error - this should produce a type error.
-    command: "NOT_A_COMMAND",
-    aggregateRootId: "PER-T4-A5",
-    // @ts-expect-error - this should produce a type error.
-    data: {
-      incorrectCommandData: true,
-    },
-  };
-});
+describe("type safety", () => {
+  it("produces type errors given an invalid aggregate type", async () => {
+    const _params: Parameters<typeof issueCommand>[0] = {
+      // @ts-expect-error - this should produce a type error.
+      aggregateRootType: "NOT_AN_AGGREGATE",
+      // @ts-expect-error - this should produce a type error.
+      command: "NOT_A_COMMAND",
+      aggregateRootId: "PER-T4-A5",
+      // @ts-expect-error - this should produce a type error.
+      data: {
+        incorrectCommandData: true,
+      },
+    };
+  });
 
-Deno.test("produces type errors given an invalid command", async () => {
-  const _params: Parameters<typeof issueCommand>[0] = {
-    aggregateRootType: "FLIGHT",
-    // @ts-expect-error - this should produce a type error.
-    command: "NOT_A_COMMAND",
-    aggregateRootId: "PER-T4-A5",
-    // @ts-expect-error - this should produce a type error.
-    data: {
-      incorrectCommandData: true,
-    },
-  };
-});
+  it("produces type errors given an invalid command", async () => {
+    const _params: Parameters<typeof issueCommand>[0] = {
+      aggregateRootType: "FLIGHT",
+      // @ts-expect-error - this should produce a type error.
+      command: "NOT_A_COMMAND",
+      aggregateRootId: "PER-T4-A5",
+      // @ts-expect-error - this should produce a type error.
+      data: {
+        incorrectCommandData: true,
+      },
+    };
+  });
 
-Deno.test("produces type errors given invalid command data", async () => {
-  const _params: Parameters<typeof issueCommand>[0] = {
-    aggregateRootType: "FLIGHT",
-    // @ts-expect-error - this should produce a type error.
-    command: "confirmPassengerBoarding",
-    aggregateRootId: "PER-T4-A5",
-    // @ts-expect-error - this should produce a type error.
-    data: {
-      incorrectCommandData: true,
-    },
-  };
+  it("produces type errors given invalid command data", async () => {
+    const _params: Parameters<typeof issueCommand>[0] = {
+      aggregateRootType: "FLIGHT",
+      // @ts-expect-error - this should produce a type error.
+      command: "confirmPassengerBoarding",
+      aggregateRootId: "PER-T4-A5",
+      // @ts-expect-error - this should produce a type error.
+      data: {
+        incorrectCommandData: true,
+      },
+    };
+  });
 });
