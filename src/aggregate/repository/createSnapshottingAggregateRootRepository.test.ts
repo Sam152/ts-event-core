7580509c59d981bc0b8639db88c8c21bfe729e09
Sam <54m152@gmail.com>
2025-07-26 16:01:14 +0800

    Progress

--- src/aggregate/repository/createSnapshottingAggregateRootRepository.test.ts
+++ src/aggregate/repository/createSnapshottingAggregateRootRepository.test.ts
@@ -5,6 +5,7 @@ import { EventsRaisedByAggregateRoots } from "../../eventStore/EventStore.ts";
 import { createSnapshottingAggregateRootRepository } from "./createSnapshottingAggregateRootRepository.ts";
 import { traceCalls } from "../../../test/utils/traceCalls.ts";
 import { describe, it } from "jsr:@std/testing/bdd";
+import { assertEquals } from "@std/assert";
 
 describe("snapshotting aggregate root repository", () => {
   const { proxy: eventStore, calls: eventStoreCalls } = traceCalls(
@@ -36,11 +37,43 @@ describe("snapshotting aggregate root repository", () => {
       ],
     });
 
-    await aggregateRootRepository.retrieve({
+    await eventStore.persist([
+      {
+        aggregateVersion: 3,
+        aggregateRootId: "VA-497",
+        aggregateRootType: "FLIGHT",
+        recordedAt: new Date(),
+        payload: {
+          type: "PASSENGER_BOARDED",
+          passengerName: "Fred Gribble",
+          passportNumber: "PA4567",
+        },
+      },
+    ]);
+
+    const aggregate = await aggregateRootRepository.retrieve({
       aggregateRootId: "VA-497",
       aggregateRootType: "FLIGHT",
     });
 
-    console.log(eventStoreCalls);
+    // The event store is only required to retrieve events from version 2 of the aggregate,
+    // satisfying the rest of the requirements from the snapshot storage.
+    assertEquals(eventStoreCalls[2], {
+      name: "retrieve",
+      args: [
+        {
+          aggregateRootId: "VA-497",
+          aggregateRootType: "FLIGHT",
+          fromVersion: 2,
+        },
+      ],
+    });
+
+    assertEquals(aggregate.state, {
+      totalSeats: 100,
+      totalBoardedPassengers: 2,
+      passengerManifest: { PA1234567: "Harold Gribble", PA4567: "Fred Gribble" },
+      status: "ON_THE_GROUND",
+    });
   });
 });
