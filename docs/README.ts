47d353b99b9b40027e798c948dca3cf348c2f7bf
Sam <54m152@gmail.com>
2025-08-03 21:42:59 +0800

    Progress

--- docs/README.ts
+++ docs/README.ts
@@ -9,7 +9,7 @@ export function README(): string {
     This project is a reference implementation of event sourcing implemented in TypeScript
     using a functional programming style.
     
-    It contains a series of loosely coupled types which show how the core of an event
+    It contains a set of loosely coupled types which show how the core of an event
     sourced system might fit together and various implementations of each type. Provided
     these types can be satisfied, components can be swapped and interchanged.
     
