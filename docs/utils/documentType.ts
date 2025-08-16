67e2068ab1ea132e8a0704f204ed5a3fbb10a811
Sam <54m152@gmail.com>
2025-08-16 10:47:03 +0800

    Progress

--- docs/utils/documentType.ts
+++ docs/utils/documentType.ts
@@ -4,15 +4,6 @@ import { padAfterFirstLine } from "./padAfterFirstLine.ts";
 import { extractSymbolAndDocString } from "./extractSymbolAndDocString.ts";
 import { pipe } from "./pipe.ts";
 
-/**
- * This function should:
- *
- * - Take filePath, read the contents of the file into a string.
- * - Scan the file line by line until the line contains the string: `type ${typeName}`.
- * - Read down until a line matching is found /^((\}\;)|(\>\;)/, that is the body of the type.
- * - Read up until /** is encountered, that is the docstring of the type.
- * - Create variables typeBody and docstring out of these components.
- */
 function doDocumentType<TType>(): string {
   const { filePath, typeName } = fileContainingGenericType({
     callSites: getCallSites(),
@@ -27,9 +18,11 @@ function doDocumentType<TType>(): string {
   return docString ? `${docString}\n\n\n${symbolBody}` : symbolBody;
 }
 
-export function documentType<TType>() {
-  return pipe(
+/**
+ * Padded to align with existing Readme indent.
+ */
+export const documentType = <TType>() =>
+  pipe(
     doDocumentType,
     padAfterFirstLine({ count: 4, char: " " }),
   )("");
-}
