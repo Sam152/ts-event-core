14035c8e40eb5695fa82c7a4f93f2606d742d251
Sam <54m152@gmail.com>
2025-08-16 10:00:14 +0800

    Progress

--- docs/utils/documentType.ts
+++ docs/utils/documentType.ts
@@ -18,32 +18,27 @@ export function documentType<TType>(): string {
   const lines = fileContents.split("\n");
 
   const typeStartIndex = lines.findIndex((line) => line.includes(`type ${typeName}`));
-
   if (typeStartIndex === -1) {
     throw new Error(`Type ${typeName} not found in ${filePath}`);
   }
-
   const typeEndIndex = lines
     .slice(typeStartIndex + 1)
     .findIndex((line) => /^((\}\;)|(\>\;))/.test(line.trim())) + typeStartIndex + 1;
-
   if (typeEndIndex === typeStartIndex) {
     throw new Error(`Type ${typeName} end not found in ${filePath}`);
   }
+  const typeBody = lines.slice(typeStartIndex, typeEndIndex + 1).join("\n");
 
   const docstringStartIndex = lines
     .slice(0, typeStartIndex)
     .reverse()
     .findIndex((line) => line.trim().includes("/**"));
-
-  const actualDocstringStartIndex = docstringStartIndex !== -1
+  const actualDocstringStartIndex: number | undefined = docstringStartIndex !== -1
     ? typeStartIndex - 1 - docstringStartIndex
-    : -1;
-
-  const typeBody = lines.slice(typeStartIndex, typeEndIndex + 1).join("\n");
-  const docstring = actualDocstringStartIndex !== -1
+    : undefined;
+  const docstring: string | undefined = actualDocstringStartIndex
     ? lines.slice(actualDocstringStartIndex, typeStartIndex).join("\n")
-    : "";
+    : undefined;
 
   const output = docstring ? `${docstring}\n${typeBody}` : typeBody;
 
