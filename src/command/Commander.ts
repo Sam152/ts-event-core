f73717217eda57fea0cbabbbe83f710a143df4a1
Sam Becker <820219+Sam152@users.noreply.github.com>
2025-08-29 15:42:52 +0800

    Progress

--- src/command/Commander.ts
+++ src/command/Commander.ts
@@ -1,4 +1,7 @@
-import { AggregateRootDefinitionMap, AggregateRootDefinitionMapTypes, } from "../aggregate/AggregateRootDefinition.ts";
+import {
+  AggregateRootDefinitionMap,
+  AggregateRootDefinitionMapTypes,
+} from "../aggregate/AggregateRootDefinition.ts";
 
 /**
  * When issuing a command...
