3d1f659df6879bb49262f37125e360662a589408
Sam Becker <820219+Sam152@users.noreply.github.com>
2025-08-29 16:47:07 +0800

    Progress

--- test/airlineDomain/aggregateRoot/flight/commands/confirmTakeOff.ts
+++ test/airlineDomain/aggregateRoot/flight/commands/confirmTakeOff.ts
@@ -4,6 +4,7 @@ import { FlightEvent } from "../state/FlightEvent.ts";
 export function confirmTakeOff(
   flight: FlightState,
 ): FlightEvent {
+  // Move to "withInServiceFlight" HOF, which returns failure event.
   assertPlaneInService(flight);
 
   if (flight.status === "IN_THE_AIR") {
