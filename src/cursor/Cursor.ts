b14f274dd50cd03d5d6b79354cfc582c13761419
Sam Becker <820219+Sam152@users.noreply.github.com>
2025-08-29 21:54:33 +0800

    Progress

--- src/cursor/Cursor.ts
+++ src/cursor/Cursor.ts
@@ -2,12 +2,10 @@ import { Event } from "../eventStore/EventStore.ts";
 
 export type Cursor<TEvent extends Event> = {
   subscribe: (name: string, subscriber: (event: TEvent) => void | Promise<void>) => void;
-}
+};
 
 function createMemoryCursor() {
-
 }
 
 export function createPersistentLockingCursor() {
-
 }
