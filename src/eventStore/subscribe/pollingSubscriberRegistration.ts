26996b5aecc23cb40eedc45efd244253b3c22a0c
Sam Becker <820219+Sam152@users.noreply.github.com>
2025-09-05 20:16:30 +0800

    Progress

---
+++ src/eventStore/subscribe/pollingSubscriberRegistration.ts
@@ -0,0 +1,18 @@
+import { Cursor } from "./Cursor.ts";
+import { Event, EventStore } from "../EventStore.ts";
+
+export function pollingSubscriberRegistration<TEvent extends Event = Event>(
+  { cursor, eventStore, pollIntervalMs }: {
+    cursor: Cursor;
+    eventStore: EventStore<TEvent>;
+    pollIntervalMs: number;
+    subscribers: Array<(event: TEvent) => Promise<void>>;
+  },
+) {
+  const pollAndProcessEvents = () => {
+    // const events = eventStore.retrieve();
+  };
+
+  setInterval(() => {
+  }, pollIntervalMs);
+}
