330dca38844fafaef8809553925d1793df94c56f
Sam <Sam152@users.noreply.github.com>
2025-10-25 15:19:43 +1100

    Progress

--- test/airlineDomain/aggregateRoot/flight/commands/registerPassengerBoarded.ts
+++ test/airlineDomain/aggregateRoot/flight/commands/registerPassengerBoarded.ts
@@ -1,11 +1,19 @@
-import { FlightState } from "../state/FlightState.ts";
-import { FlightEvent } from "../state/FlightEvent.ts";
-import { withScheduledFlight } from "../util/withScheduledFlight.ts";
+import {ScheduledFlightState} from "../state/FlightState.ts";
+import {FlightEvent} from "../state/FlightEvent.ts";
+import {withScheduledFlight} from "../util/withScheduledFlight.ts";
 
-export const registerPassengerBoarded = withScheduledFlight("REGISTER_PASSENGER_ONBOARD", (
-  flight: FlightState,
+export const registerPassengerBoarded = withScheduledFlight("REGISTER_PASSENGER_BOARDED", (
+  flight: ScheduledFlightState,
   data: { passengerName: string; passportNumber: string },
 ): FlightEvent => {
+
+  if (flight.status === 'IN_THE_AIR') {
+    return {
+      type: 'REGISTER_PASSENGER_BOARDED_FAILED',
+      reason: 'PLANE_ALREADY_IN_THE_AIR',
+    }
+  }
+
   return {
     type: "PASSENGER_BOARDED",
     passengerName: data.passengerName,
