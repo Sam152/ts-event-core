44f26345eea2c57a236875c18693f2cbfbaf9dd7
Sam <Sam152@users.noreply.github.com>
2025-10-25 14:54:17 +1100

    Progress

--- test/airlineDomain/aggregateRoot/flight/commands/confirmLanding.ts
+++ test/airlineDomain/aggregateRoot/flight/commands/confirmLanding.ts
@@ -1,8 +1,9 @@
-import { FlightState } from "../state/FlightState.ts";
-import { FlightEvent } from "../state/FlightEvent.ts";
+import {FlightState} from "../state/FlightState.ts";
+import {FlightEvent} from "../state/FlightEvent.ts";
+import {withScheduledFlight} from "../util/withScheduledFlight.ts";
 
-export function confirmLanding(_flight: FlightState): FlightEvent {
+export const confirmLanding = withScheduledFlight('CONFIRM_LANDING', (_flight: FlightState): FlightEvent => {
   return {
     type: "FLIGHT_LANDED",
   };
-}
+});
