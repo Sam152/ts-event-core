ef9555d0a7fe74e2f505d078143cd8c697cdbbe7
Sam <Sam152@users.noreply.github.com>
2025-10-25 15:20:48 +1100

    Progress

--- test/airlineDomain/aggregateRoot/gate/commands/closeGate.ts
+++ test/airlineDomain/aggregateRoot/gate/commands/closeGate.ts
@@ -1,6 +1,13 @@
-import { GateEvent } from "../state/GateEvent.ts";
+import {GateEvent} from "../state/GateEvent.ts";
+import {GateState} from "../state/GateState.ts";
 
-export function closeGate(): GateEvent {
+export function closeGate(gate: GateState): GateEvent {
+  if (gate.status !== "OPEN") {
+    return {
+      type: "OPEN_GATE_FAILED",
+      reason: "GATE_ALREADY_OPEN",
+    };
+  }
   return {
     type: "GATE_CLOSED",
   };
