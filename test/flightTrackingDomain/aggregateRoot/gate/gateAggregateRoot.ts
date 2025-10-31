7d58c1282bfdc02301b89dcafb552fa1232b50bd
Sam <Sam152@users.noreply.github.com>
2025-10-31 21:39:19 +0800

    Progress

--- test/flightTrackingDomain/aggregateRoot/gate/gateAggregateRoot.ts
+++ test/flightTrackingDomain/aggregateRoot/gate/gateAggregateRoot.ts
@@ -3,8 +3,8 @@ import { scanBoardingPass } from "./commands/scanBoardingPass.ts";
 import { gateReducer } from "./state/gateReducer.ts";
 import { GateState } from "./state/GateState.ts";
 import { GateEvent } from "./state/GateEvent.ts";
-import { openGate } from "./commands/openGate.ts";
 import { closeGate } from "./commands/closeGate.ts";
+import { openGate } from "./commands/openGate.ts";
 
 export const gateAggregateRoot = {
   state: {
