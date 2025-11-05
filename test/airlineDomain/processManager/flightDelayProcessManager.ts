47bfbf73175eceb5006c14a3844c82451cf80a9a
Sam <Sam152@users.noreply.github.com>
2025-11-05 22:08:03 +0800

    Progress

--- test/airlineDomain/processManager/flightDelayProcessManager.ts
+++ test/airlineDomain/processManager/flightDelayProcessManager.ts
@@ -2,8 +2,20 @@ import { CommandIssuer } from "@ts-event-core/framework";
 import { airlineAggregateRoots, AirlineDomainEvent } from "../index.ts";
 
 /**
- * A process manager which ensures that when a flight is delayed, each impacted
- * passenger is notified.
+ * A process manager facilitates coordination between aggregates. In this case, when a `FLIGHT_DELAYED` event is raised,
+ * we must issue a command for each of the impacted passengers, to notify them of the delay.
+ *
+ * Command processing within a single aggregate is considered strongly consistent, data passed into each command is guaranteed to
+ * be up-to-date and the outcome of a command will never be committed if it conflicts with any commands running in parallel
+ * for the same aggregate.
+ *
+ * The same does not apply between aggregates. If a flight is delayed the moment a ticket is purchased, the passenger may
+ * or may not be notified of the delay. Passengers are notified of delays at some point in the future, after a delays has been
+ * recorded. These tradeoffs are completely acceptable in our airline domain.
+ *
+ * Features which require strong consistency (such as not overbooking a flight) must be validated within a single aggregate
+ * root. If consistency issues are a headache in a given system, it may be a sign that aggregate roots have been modelled
+ * too granularly.
  */
 export async function flightDelayProcessManager(
   { event, issueCommand }: {
