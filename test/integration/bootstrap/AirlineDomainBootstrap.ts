50bc608a1afbff6f4f5c4548b85e5a0d756f88a6
Sam <Sam152@users.noreply.github.com>
2025-11-05 22:36:21 +0800

    Progress

--- test/integration/bootstrap/AirlineDomainBootstrap.ts
+++ test/integration/bootstrap/AirlineDomainBootstrap.ts
@@ -2,15 +2,42 @@ import { CommandIssuer } from "@ts-event-core/framework";
 import { airlineAggregateRoots, LifetimeEarningsReport } from "@ts-event-core/airline-domain";
 
 /**
- * A bootstrap of the flight tracking domain. This object combines components of the
- * framework with the domain and provides an interface between them.
+ * To bootstrap a domain into a working application, key framework components need to be composed together with the
+ * domain. All components have in-memory implementations, which allow for fast integration testing and persistent components
+ * where applicable, which are more suited to production.
  *
- * Consumers of the framework are expected to understand each of the components, combine
- * them in a way that sensible for their use case and integrate their own domain.
+ * The framework does not dictate the shape or properties of a bootstrap, but instead provides a library of underlying
+ * components which should be composed together depending on the use case.
  *
- * The domain code is ultimately a series of pure functions, which could be independently
- * shipped and integrated into different contexts, but is defined in a way here which
- * smoothly integrates with the framework components.
+ * The `event-sourcing-bootstrap.test.ts` test is a good reference for initializing an in-memory bootstrap and a production
+ * bootstrap, both which pass the same test case, which begins with scheduling a flight:
+ *
+ * ```typescript
+ * it("allows us to schedule a flight", async () => {
+ *   await issueCommand({
+ *     aggregateRootType: "FLIGHT",
+ *     aggregateRootId: "SB93",
+ *     command: "scheduleFlight",
+ *     data: {
+ *       departureTime: new Date("2035-01-01T05:00:00Z"),
+ *       sellableSeats: 3,
+ *     },
+ *   });
+ * });
+ * ```
+ *
+ * ...and concludes asserting notifications were sent:
+ *
+ * ```typescript
+ * it("ensures notifications are sent to affected passengers through the correct channel", async () => {
+ *   await tryThing(() =>
+ *     assertArrayIncludes(notificationLog, [
+ *       "EMAIL: sam@example.com Flight delayed Hi, Flight SB93 has been delayed. Sorry about that.",
+ *       "SMS: +61491570158 Uh-oh! Flight SB93 has been delayed... we're sorry :(",
+ *     ])
+ *   );
+ * });
+ * ```
  */
 export type AirlineDomainBootstrap = {
   issueCommand: CommandIssuer<typeof airlineAggregateRoots>;
