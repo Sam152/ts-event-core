c8ad6f8c6afabcb19c33a5d3b32537921b813b10
Sam <54m152@gmail.com>
2025-11-09 13:00:40 +0800

    Progress

--- src/eventStore/createInMemoryEventStore.ts
+++ src/eventStore/createInMemoryEventStore.ts
@@ -2,27 +2,20 @@ import { Event, EventStore, PersistedEvent } from "./EventStore.ts";
 import { AggregateRootVersionIntegrityError } from "./error/AggregateRootVersionIntegrityError.ts";
 
 type EventSubscriber<TEvent extends Event> = (event: TEvent) => Promise<void> | void;
-type EventEmitter<TEvent extends Event> = {
-  addSubscriber: (subscriber: EventSubscriber<TEvent>) => void;
-};
 
 /**
  * An in-memory test store is most useful for testing purposes. Most use cases
  * would benefit from persistent storage.
  */
-export function createInMemoryEventStore<TEvent extends Event>():
-  & EventStore<TEvent>
-  & EventEmitter<TEvent> {
+export function createInMemoryEventStore<TEvent extends Event>(): EventStore<TEvent> {
   const storageByAggregate: Record<string, PersistedEvent<TEvent>[]> = {};
   const storageByInsertOrder: PersistedEvent<TEvent>[] = [];
-  const subscribers: EventSubscriber<TEvent>[] = [];
 
   let idSequence = 0;
 
   return {
-    addSubscriber: subscribers.push.bind(subscribers),
     persist: async (events) => {
-      await Promise.all(events.map(async (event) => {
+      events.forEach((event) => {
         const key = streamKey(event.aggregateRootType, event.aggregateRootId);
         storageByAggregate[key] = storageByAggregate[key] ?? [];
 
@@ -40,9 +33,7 @@ export function createInMemoryEventStore<TEvent extends Event>():
 
         storageByAggregate[key].push(persistedEvent);
         storageByInsertOrder.push(persistedEvent);
-
-        await Promise.all(subscribers?.map((subscriber) => subscriber(event)) ?? []);
-      }));
+      });
     },
     retrieve: async function* ({
       aggregateRootType,
