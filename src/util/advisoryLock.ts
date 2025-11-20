b84c15014754714e3c6b9f4a0788fabf7e868b04
Sam <54m152@gmail.com>
2025-11-20 16:04:38 +0800

    Progress

--- src/util/advisoryLock.ts
+++ src/util/advisoryLock.ts
@@ -1,10 +1,14 @@
 import type postgres from "postgres";
+import { createHash } from "node:crypto";
 
 export async function advisoryLock(
   { id, connection }: { id: string; connection: ReturnType<typeof postgres> },
 ): Promise<() => Promise<void>> {
   const txn = await connection.reserve();
-  await txn`BEGIN; SELECT pg_advisory_xact_lock(('x' || md5('${id}'))::bit(64)::bigint);`.simple();
+  // Hash the ID, to ensure it's plain, since this is passed directly into postgres without
+  // parameterization.
+  const hashedId = createHash("md5").update(id).digest("hex");
+  await txn`BEGIN; SELECT pg_advisory_xact_lock(('x' || md5('${hashedId}'))::bit(64)::bigint);`.simple();
   return async () => {
     await txn`COMMIT`;
     txn.release();
